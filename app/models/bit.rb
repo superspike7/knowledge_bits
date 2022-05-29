394a50ed239a3da12312bf5a7d8ae2ead472d8c2
Spike Vinz <spikevinz@gmail.com>
2022-05-29 14:17:23 +0800

    add basic bit validations
    add error messages on form

--- app/models/bit.rb
+++ app/models/bit.rb
@@ -1,2 +1,9 @@
 class Bit < ApplicationRecord
+  validates_presence_of :title, :body
+  validates :body, length: { maximum: 250,
+                             too_long: '%{count} characters is the maximum allowed' }
+
+  before_create do
+    self.author = "Anonymous" if author.blank?
+  end
 end
