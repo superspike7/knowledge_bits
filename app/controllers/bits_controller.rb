7b948aa33654764e1e87ede12ac24455e552da84
Spike Vinz <spikevinz@gmail.com>
2022-05-29 13:31:08 +0800

    add Create in CRUD
    fix navlink typo

--- app/controllers/bits_controller.rb
+++ app/controllers/bits_controller.rb
@@ -9,8 +9,31 @@ class BitsController < ApplicationController
   end
 
   def new
+    @bit = Bit.new
   end
 
   def edit
+  end
+
+  def create
+    @bit = Bit.new(bit_params)
+
+    if @bit.save
+      redirect_to @bit 
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def bit_params
+    params.require(:bit).permit(:title, :author, :body)
   end
 end
